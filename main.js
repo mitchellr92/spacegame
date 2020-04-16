350a324fbde54edb5d7df5abe4cc79a815494310
Mitchell Robles <mitchellr92@live.com>
2020-04-16 10:07:06 -0700

    ship placeholder drawn on canvas

--- main.js
+++ main.js
@@ -6,6 +6,30 @@ const ctx = canvas.getContext("2d");
 
 let score = 0;
 
+const ship = {
+  x: canvas.width / 2,
+  y: canvas.height / 2,
+  size: 10,
+  speed: 8,
+  dx: 0
+};
+
+// Draw ship on canvas
+function drawShip() {
+  ctx.beginPath();
+  ctx.arc(ship.x, ship.y, ship.size, 0, Math.PI * 2);
+  ctx.fillStyle = "#0095dd";
+  ctx.fill();
+  ctx.closePath();
+}
+
+// Draw everything
+function draw() {
+  drawShip();
+}
+
+draw();
+
 // Event handlers
 rulesBtn.addEventListener("click", () => {
   console.log("click");
