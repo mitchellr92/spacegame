d18fabe80b09874467410c4f62c827e3a3c7a48d
Mitchell Robles <mitchellr92@live.com>
2020-04-20 16:50:25 -0700

    missile fired on key down for the enter key

--- main.js
+++ main.js
@@ -6,12 +6,22 @@ const ctx = canvas.getContext("2d");
 
 let score = 0;
 
+// Create ship props
 const ship = {
   x: canvas.width / 2,
   y: canvas.height - 40,
   size: 10,
-  speed: 8,
+  speed: 4,
   dx: 0
+};
+
+// Create missile props
+const missile = {
+  x: canvas.width / 2,
+  y: canvas.height - 40,
+  size: 4,
+  speed: 15,
+  dy: -7
 };
 
 // Draw ship on canvas
@@ -23,16 +33,35 @@ function drawShip() {
   ctx.closePath();
 }
 
+// Draw missile on canvas
+function drawMissile() {
+  ctx.beginPath();
+  ctx.arc(missile.x, missile.y, missile.size, 0, Math.PI * 2);
+  ctx.fillStyle = "#0095dd";
+  ctx.fill();
+  ctx.closePath();
+}
+
 // Draw everything
 function draw() {
+  // Clear canvas
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+  drawMissile();
   drawShip();
+}
+
+// Move missile on canvas
+function moveMissile() {
+  missile.y += missile.dy;
+  requestAnimationFrame(moveMissile);
 }
 
 // Move ship on canvas
 function moveShip() {
   ship.x += ship.dx;
 
-  // Wall detection
+  // Wall collision (right/left)
   if (ship.x + ship.size > canvas.width) {
     ship.x = canvas.width - ship.size;
   }
@@ -42,14 +71,26 @@ function moveShip() {
   }
 }
 
-// Keydown event
+// Update canvas drawing and animation
+function update() {
+  moveShip();
+
+  // Draw everything
+  draw();
+
+  requestAnimationFrame(update);
+}
+
+update();
+
 function keyDown(e) {
   if (e.key === "a") {
     ship.dx = -ship.speed;
   } else if (e.key === "d") {
     ship.dx = ship.speed;
-  } else if (e.key === " ") {
-    console.log("space");
+  } else if (e.key === "Enter") {
+    console.log("Fire!");
+    moveMissile();
   }
 }
 
@@ -59,17 +100,6 @@ function keyUp(e) {
     ship.dx = 0;
   }
 }
-
-// Update canvas drawing and animation
-function update() {
-  moveShip();
-
-  draw();
-
-  requestAnimationFrame(update);
-}
-
-update();
 
 // Event handlers
 rulesBtn.addEventListener("click", () => {
